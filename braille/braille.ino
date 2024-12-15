65c4dd87e26c5aa9c6c9dfca0d5400a132e07480
mtalirfan <mtalirfan@gmail.com>
2024-12-15 18:31:12 +0500

    Modified display loops on dot, cell, display level
    hello world !

--- braille/braille.ino
+++ braille/braille.ino
@@ -5,12 +5,14 @@
 #include "braille_dictionaries.h"
 
 // global variables
-int first_pin = 22; // right-hand side pins of Arduino MEGA, 46 for cell 5
+int first_pin = 22; // right-hand side pins of Arduino MEGA, 22 for cell 1, 46 for cell 5
 
-int timer = 500; // ms for cell
+int n_dots = 6; // 6-dot braille
+int n_cells = 4; // 0 to 5
 
-int n_dots = 6;
-int n_cells = 5; // 0 to 5
+int timer_dot = 0; // ms for single dot
+int timer_cell = 100; // ms for single cell of n_dots
+int timer_display = 4000; // ms for display of n_cells
 
 // function declarations
 void display_braille(String brailled_text);
@@ -23,13 +25,14 @@ void setup() {
         pinMode(pin, OUTPUT);
     }
 
-    String brailled_text = "100000 101100 110000 100000 101100 110000 100000 101100 110000 000010 011011 000100 100011";
-
-    display_braille(brailled_text);
-
 }
 
+String brailled_text = "110010 100010 111000 111000 101010 000000 000001 000111 010111";
+
+
 void loop() {
+
+    display_braille(brailled_text);
 
 }
 
@@ -59,34 +62,64 @@ void display_braille(String brailled_text) {
     String cell = "";
 
     // Display brailled list
+    for (int i = 0; i <= floor(brailled_list_length / n_cells); i++) {
+        for (int j = 0; j < n_cells; j++) {
 
-    // loop count must be a multiple of n_cells
-    for (int i = 0; i < (brailled_list_length + ( n_cells - (brailled_list_length % n_cells) ) ) ; i++) {
+            if ((i * n_cells + j) >= brailled_list_length) {
+              cell = "000000"; // remainder at the final n_cells loop
+            } else {
+              cell = brailled_list[i * n_cells + j];
+            }
 
-        if (i >= (brailled_list_length) ) {
-          cell = "000000"; // remainder at the final n_cells loop
-        } else {
-          cell = brailled_list[i];
+            // Get unicode character
+            if (unicode.find(cell) != unicode.end()) {
+                braille_unicode += unicode[cell];
+                braille_unicode_spaced += String("  ") + unicode[cell] + String("  ");
+            }
+
+            // Build braille text lines
+            braille_text_1 += String(" ") + cell.charAt(0) + String(" ") + cell.charAt(3) + String(" ");
+            braille_text_2 += String(" ") + cell.charAt(1) + String(" ") + cell.charAt(4) + String(" ");
+            braille_text_3 += String(" ") + cell.charAt(2) + String(" ") + cell.charAt(5) + String(" ");
+
+            // Set pins to display braille cell on LEDs
+            for (int k = 0; k < n_dots; k++) {
+                digitalWrite(first_pin + j * n_dots + k, cell.charAt(k) - '0');
+                delay(timer_dot);
+            }
+            delay(timer_cell);
         }
-
-        // Get unicode character
-        if (unicode.find(cell) != unicode.end()) {
-            braille_unicode += unicode[cell];
-            braille_unicode_spaced += String("  ") + unicode[cell] + String("  ");
-        }
-
-        // Build braille text lines
-        braille_text_1 += String(" ") + cell.charAt(0) + String(" ") + cell.charAt(3) + String(" ");
-        braille_text_2 += String(" ") + cell.charAt(1) + String(" ") + cell.charAt(4) + String(" ");
-        braille_text_3 += String(" ") + cell.charAt(2) + String(" ") + cell.charAt(5) + String(" ");
-
-        // Set pins to display braille cell on LEDs
-        for (int j = 0; j < n_dots; j++) {
-            digitalWrite(first_pin + (i % n_cells) * n_dots + j, cell.charAt(j) - '0');
-        }
-
-        delay(timer);
+        delay(timer_display);
     }
+
+    // CELL-BY-CELL LOOP
+    // // loop count must be a multiple of n_cells
+    // for (int i = 0; i < (brailled_list_length + ( n_cells - (brailled_list_length % n_cells) ) ) ; i++) {
+
+    //     if (i >= (brailled_list_length) ) {
+    //       cell = "000000"; // remainder at the final n_cells loop
+    //     } else {
+    //       cell = brailled_list[i];
+    //     }
+
+    //     // Get unicode character
+    //     if (unicode.find(cell) != unicode.end()) {
+    //         braille_unicode += unicode[cell];
+    //         braille_unicode_spaced += String("  ") + unicode[cell] + String("  ");
+    //     }
+
+    //     // Build braille text lines
+    //     braille_text_1 += String(" ") + cell.charAt(0) + String(" ") + cell.charAt(3) + String(" ");
+    //     braille_text_2 += String(" ") + cell.charAt(1) + String(" ") + cell.charAt(4) + String(" ");
+    //     braille_text_3 += String(" ") + cell.charAt(2) + String(" ") + cell.charAt(5) + String(" ");
+
+    //     // Set pins to display braille cell on LEDs
+    //     for (int j = 0; j < n_dots; j++) {
+    //         digitalWrite(first_pin + (i % n_cells) * n_dots + j, cell.charAt(j) - '0');
+    //     }
+
+    //     delay(timer);
+    // }
 
     // Printing the braille text
     Serial.println(brailled_text);
