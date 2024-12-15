197c53907df09f688de8af3c1847bdd96399bb42
mtalirfan <mtalirfan@gmail.com>
2024-12-15 15:51:18 +0500

    Working and customisable display_braille function

--- braille/braille.ino
+++ braille/braille.ino
@@ -5,17 +5,17 @@
 #include "braille_dictionaries.h"
 
 // global variables
-int first_pin = 22; // right-hand side pins of Arduino MEGA
+int first_pin = 22; // right-hand side pins of Arduino MEGA, 46 for cell 5
 
-int timer = 1000; // ms for dot switch
+int timer = 1000; // ms for cell
 
 int n_dots = 6;
-int n_cells = 1; // 0 to 5
+int n_cells = 4; // 0 to 5
 
 int br[7] = {0, 0, 0, 0, 0, 0, 0}; // index 0 unused
 
 // function declarations
-String display_braille(String brailled_text);
+void display_braille(String brailled_text);
 
 void setup() {
 
@@ -36,51 +36,57 @@ void loop() {
 }
 
 // functions
-String display_braille(String brailled_text) {
-  // Splitting the brailled_text into a list
-  String brailled_list[100]; // Assuming a maximum of 100 elements
-  int brailled_list_length = 0;
-  int lastIndex = 0;
-  for (int i = 0; i < brailled_text.length(); i++) {
-    if (brailled_text.charAt(i) == ' ') {
-      brailled_list[brailled_list_length] = brailled_text.substring(lastIndex, i);
-      brailled_list_length++;
-      lastIndex = i + 1;
+void display_braille(String brailled_text) {
+    // Splitting the brailled_text into a list
+    String brailled_list[100]; // Assuming a maximum of 100 elements
+    int brailled_list_length = 0;
+    int lastIndex = 0;
+    for (int i = 0; i < brailled_text.length(); i++) {
+        if (brailled_text.charAt(i) == ' ') {
+            brailled_list[brailled_list_length] = brailled_text.substring(lastIndex, i);
+            brailled_list_length++;
+            lastIndex = i + 1;
+        }
     }
-  }
-  brailled_list[brailled_list_length] = brailled_text.substring(lastIndex);
-  brailled_list_length++;
+    brailled_list[brailled_list_length] = brailled_text.substring(lastIndex);
+    brailled_list_length++;
 
-  // Variables to store braille text
-  String braille_text_1 = "";
-  String braille_text_2 = "";
-  String braille_text_3 = "";
-  String braille_unicode_spaced = "";
-  String braille_unicode = "";
+    // Variables to store braille text
+    String braille_text_1 = "";
+    String braille_text_2 = "";
+    String braille_text_3 = "";
+    String braille_unicode_spaced = "";
+    String braille_unicode = "";
 
-  // Display brailled list
-  for (int i = 0; i < brailled_list_length; i++) {
-    if (unicode.find(brailled_list[i]) != unicode.end()) {
-      braille_unicode += unicode[brailled_list[i]];
-      braille_unicode_spaced += String("  ") + unicode[brailled_list[i]] + String("  ");
+    // Display brailled list
+    for (int i = 0; i < brailled_list_length; i++) {
+        String cell = brailled_list[i];
+
+        // Get unicode character
+        if (unicode.find(cell) != unicode.end()) {
+            braille_unicode += unicode[cell];
+            braille_unicode_spaced += String("  ") + unicode[cell] + String("  ");
+        }
+
+        // Build braille text lines
+        braille_text_1 += String(" ") + cell.charAt(0) + String(" ") + cell.charAt(3) + String(" ");
+        braille_text_2 += String(" ") + cell.charAt(1) + String(" ") + cell.charAt(4) + String(" ");
+        braille_text_3 += String(" ") + cell.charAt(2) + String(" ") + cell.charAt(5) + String(" ");
+
+        // Set pins to display braille cell on LEDs
+        for (int j = 0; j < 6; j++) {
+            digitalWrite(first_pin + i * 6 + j, cell.charAt(j) - '0');
+        }
+
+        delay(timer);
     }
 
-    // Build braille text lines
-    String br = brailled_list[i];
-    braille_text_1 += String(" ") + br.charAt(0) + String(" ") + br.charAt(3) + String(" ");
-    braille_text_2 += String(" ") + br.charAt(1) + String(" ") + br.charAt(4) + String(" ");
-    braille_text_3 += String(" ") + br.charAt(2) + String(" ") + br.charAt(5) + String(" ");
-
-    delay(timer);
-  }
-
-  // Printing the braille text
-  Serial.println(brailled_text);
-
-  Serial.println(braille_text_1);
-  Serial.println(braille_text_2);
-  Serial.println(braille_text_3);
-  Serial.println(braille_unicode_spaced);
-  Serial.println("");
-  Serial.println(braille_unicode);
+    // Printing the braille text
+    Serial.println(brailled_text);
+    Serial.println(braille_text_1);
+    Serial.println(braille_text_2);
+    Serial.println(braille_text_3);
+    Serial.println(braille_unicode_spaced);
+    Serial.println("");
+    Serial.println(braille_unicode);
 }
---
+++ braille_cell_loop/braille_cell_loop.ino
@@ -0,0 +1,31 @@
+
+// global variables
+int first_pin = 22; // right-hand side pins of Arduino MEGA, 46 for cell 5
+
+int timer = 100; // ms for dot switch
+
+int n_dots = 6;
+int n_cells = 4; // 0 to 5
+
+void setup() {
+
+  for (int pin = first_pin; pin < (first_pin + n_dots * n_cells); pin++) {
+    pinMode(pin, OUTPUT);
+  }
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  for (int pin = first_pin; pin < (first_pin + n_dots * n_cells); pin++) {
+    digitalWrite(pin, HIGH);
+    delay(timer);
+  }
+
+  for (int pin = first_pin; pin < (first_pin + n_dots * n_cells); pin++) {
+    digitalWrite(pin, LOW);
+    delay(timer);
+  }
+
+}
